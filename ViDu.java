80669abea9b5c806a908adb58f92bcdb24d8c329
DatDatVT <124223373+DatDatVT@users.noreply.github.com>
2024-04-19 09:33:02 +0700

    Add files via upload

---
+++ ViDu.java
@@ -0,0 +1,21 @@
+public class ViDu {
+    public static int tinhTong(int a, int b) {
+        return a + b;
+    }
+
+    public static int tinhHieu(int a, int b) {
+        return a - b;
+    }
+
+    public static int tinhTich(int a, int b) {
+        return a * b;
+    }
+
+    public static float tinhThuong(int a, int b) {
+        return (float) a / b;
+    }
+
+    public static int tinhDu(int a, int b) {
+        return a % b;
+    }
+}
